253a48cbfff8befa2530be868e927ceab059e373
jjobJunior <jobp.s.junior@gmail.com>
2022-05-02 16:30:01 -0300

    Estruturando o projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contato from './components/pages/Contato'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container';
 
 function App() {
   return (
-    <div className="App">
-    <p>Gerenciador de Projetos</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/contato">Contato</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
+        <Container customClass="min-height">
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="Company" element={<Company />} />
+          <Route path="Contato" element={<Contato />} />
+          <Route path="Newproject" element={<NewProject />} />
+      </Routes>
+        </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
