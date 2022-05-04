3423c98844a476cee3b02bd86f9b3cde8d2d7c6b
jjobJunior <jobp.s.junior@gmail.com>
2022-05-04 15:10:47 -0300

    Criando e estilizando pagina Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,15 @@
+import styles from './Home.module.css';
+import savings from '../../img/savings.svg';
+import LinkButton from '../layout/LinkButton';
+
 function Home() {
-  return <h1>Home</h1>
+  return (
+    <section className={styles.home_container}>
+      <h1>Bem vindo ao <span>gerenciador de projetos</span></h1>
+      <p>Comece a gerenciar seus projetos agora mesmo</p>
+      <LinkButton to="/NewProject" text="Criar projeto" />
+      <img src={savings} alt="Genernciador" />
+    </section>
+  );
 }
 export default Home;
