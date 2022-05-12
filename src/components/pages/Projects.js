bd7149e1d702501a3b9c49ed463c42836b9a4b34
jjobJunior <jobp.s.junior@gmail.com>
2022-05-12 15:48:01 -0300

    Criando Dashboard e Buscando projetos da base de dados

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,16 +1,47 @@
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-
+import styles from './Projects.module.css';
 import Message from '../layout/Message';
+import Container from '../layout/Container';
+import LinkButton from '../layout/LinkButton';
+import ProjectCard from '../project/ProjectCard';
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/projects', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+        setProjects(data)
+      })
+      .catch(error => { console.log(error) })
+  }, []);
+
   const location = useLocation();
   let message = location.state ? location.state.message : null;
-
   return (
-    <div>
-      <h1>Meus Projetos</h1>
-      {message && (<Message type="success" msg={message} />
-      )}
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/NewProject" text="Criar Projeto" />
+      </div>
+      {message && (<Message type="success" msg={message} />)}
+      <Container customClass="start">
+        {projects.length > 0 && 
+        projects.map((project) => (<ProjectCard
+        key={project.id}
+        id={project.id}
+        name={project.name}
+        budget={project.budget}
+        category={project.category.name} />))}
+      </Container>
     </div>
   );
 }
